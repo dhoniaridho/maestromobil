69c2bac02677ced6e6b77dc4f5d28219884505d1
dhoniaridho <dhoniaridho@gmail.com>
2021-08-19 15:28:30 +0700

    init

--- components/layouts/navbar.js
+++ components/layouts/navbar.js
@@ -1,18 +1,26 @@
+import { Brand } from 'assets'
 import Link from 'next/link'
+import Image from 'next/image'
+import {useRouter} from 'next/router'
 
 export default function MainNavbar() {
+
+    const {asPath} = useRouter()
+
     return(
         <header>
-            <nav className="h-16 flex justify-between place-items-center">
-                <div>
-                    logo
-                </div>
+            <nav className="h-20 flex justify-between place-items-center px-5">
+                <Link href="/">
+                    <a>
+                        <Image draggable={false} className="object-scale-down" alt="brand" width="150px" src={Brand} />
+                    </a>
+                </Link>
                 <ul className="flex">
                     {navs.map((nav, i)=> {
                         return(
                             <li className="mx-1" key={i}>
                                 <Link href={nav.href}>
-                                    <a>{nav.name}</a>
+                                    <a className={`${asPath === nav.href && 'text-blue-500'}`}>{nav.name}</a>
                                 </Link>
                             </li>
                         )
