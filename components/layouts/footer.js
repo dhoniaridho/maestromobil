77e114234084f5aeb189d3b0b986fa5aaa0879ac
Ahmad Ridhoni <60641970+dhoniaridho@users.noreply.github.com>
2021-08-20 20:30:55 +0700

    rel="noreferrer"

--- components/layouts/footer.js
+++ components/layouts/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return(
         <footer>
             <div className="flex justify-center md:justify-end place-items-center h-16 border-t md:px-20">
-                <p className="text-sm"> &copy; {new Date().getFullYear()} Maestro Mobil. Created by <a href="http://dhoniaridho.vercel.app" className="hover:text-blue-600 text-blue-500" target="_blank" rel="noopener">dhoniaridho</a></p>
+                <p className="text-sm"> &copy; {new Date().getFullYear()} Maestro Mobil. Created by <a href="http://dhoniaridho.vercel.app" className="hover:text-blue-600 text-blue-500" target="_blank" rel="noreferrer">dhoniaridho</a></p>
             </div>
         </footer>
     )
