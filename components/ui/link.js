4c72c002b409f5e70d1ee24bda93f4aff51a8d3c
Ahmad Ridhoni <60641970+dhoniaridho@users.noreply.github.com>
2021-08-21 22:09:00 +0700

    Update link.js

--- components/ui/link.js
+++ components/ui/link.js
@@ -4,7 +4,7 @@ import NextLink from "next/link"
 export default function Link(props) {
     const {href, children, type, size, className} = props
 
-    const classN = classNames(`px-6 py-3 text-center ${className}`, {
+    const classN = classNames(`px-6 py-3 text-center whitespace-nowrap ${className}`, {
         'bg-orange-500 hover:bg-orange-600 text-white' : type === 'secondary',
         'bg-cyan-500 hover:bg-cyan-600 text-white' : type === 'primary',
         'bg-${color}-500 hover:bg-${color}-600 text-white' : type === 'primary',
